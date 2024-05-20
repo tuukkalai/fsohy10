2db2f256761bba4311455adfeb88895c6b62f3e2
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2024-05-20 14:07:33 +0300

    add loading text while repositories are being fetched

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { FlatList, View, StyleSheet } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
+import Text from "./Text";
 
 const styles = StyleSheet.create({
   content: {
@@ -15,11 +16,24 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const { repositories, loading } = useRepositories();
 
   const repoNodes = repositories ? repositories.edges.map((e) => e.node) : [];
 
   return (
+    <>
+      {loading ? (
+        <Text>Loading</Text>
+      ) : (
+        <FlatList
+          style={styles.content}
+          data={repoNodes}
+          ItemSeparatorComponent={ItemSeparator}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <RepositoryItem item={item} />}
+        />
+      )}
+    </>
   );
 };
 
