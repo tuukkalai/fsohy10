7e5efc15544e082b50e552045918304ada4a8caa
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2025-01-10 23:38:23 +0200

    (1015): store access token

--- src/hooks/useAuthStorage.js
+++ src/hooks/useAuthStorage.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import AuthStorageContext from "../contexts/AuthStorageContext";
 
 const useAuthStorage = () => {
-    return useContext(AuthStorageContext);
-}
+  return useContext(AuthStorageContext);
+};
 
 export default useAuthStorage;
