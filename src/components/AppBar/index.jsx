ad9a1f983d06fbac3dce9e30026e0e37c8cacd8d
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2026-04-16 11:36:45 +0300

    feat: add sign out

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -3,15 +3,49 @@ import Constants from "expo-constants";
 import theme from "../../theme";
 import AppBarTab from "./AppBarTab";
 
+import { useQuery } from "@apollo/client";
+import { WHOAMI } from "../../graphql/queries";
+
+import { Pressable } from "react-native";
+import Text from "../Text";
+
+import { useApolloClient } from "@apollo/client";
+import useAuthStorage from "../../hooks/useAuthStorage";
+
 const styles = StyleSheet.create({
   container: {
     padding: 10,
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.color.primaryBg,
   },
+  nav: {
+    padding: 8,
+  },
 });
 
+const SignOutTab = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const handleSignOut = () => {
+    authStorage.removeAccessToken();
+    apolloClient.resetStore();
+  };
+  return (
+    <Pressable onPress={handleSignOut}>
+      <Text
+        style={styles.nav}
+        fontSize="medium"
+        fontWeight="bold"
+      >
+        Sign Out
+      </Text>
+    </Pressable>
+  );
+};
+
 const AppBar = () => {
+  const { loading, data } = useQuery(WHOAMI, { fetchPolicy: "network-only" });
+  const currentUser = data?.me;
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
@@ -19,10 +53,14 @@ const AppBar = () => {
           title="Repositories"
           to="/"
         />
-        <AppBarTab
-          title="Sign In"
-          to="/signin"
-        />
+        {!loading && currentUser ? (
+          <SignOutTab />
+        ) : (
+          <AppBarTab
+            title="Sign In"
+            to="/signin"
+          />
+        )}
       </ScrollView>
     </View>
   );
